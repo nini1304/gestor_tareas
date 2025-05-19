e6253df0aafe124d78018af862eb94287ece592b
Geraldine Ibieta <geraldine.ibieta13@gmail.com>
2025-05-19 18:30:02 -0400

    Pequeño arreglo

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
+require('dotenv').config(); 
 const app = require('./app');
 const http = require('http');
 
 const server = http.createServer(app);
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.DB_PORT || 3000;
 server.listen(PORT, () => {
   console.log(`🚀 Servidor corriendo en http://localhost:${PORT}`);
 });
