c7e449fbe2fd16743d5a3e946d60cd8b85d742eb
Geraldine Ibieta <geraldine.ibieta13@gmail.com>
2025-05-19 18:19:48 -0400

    Cambios para el deploy

---
+++ config/config.js
@@ -0,0 +1,34 @@
+require('dotenv').config();
+
+module.exports = {
+  development: {
+    username: process.env.DB_USER || "postgres",
+    password: process.env.DB_PASS || "postgres",
+    database: process.env.DB_NAME || "gestor_de_tareas",
+    host: process.env.DB_HOST || "127.0.0.1",
+    port: process.env.DB_PORT || 5433,
+    dialect: 'postgres',
+  },
+  test: {
+    username: process.env.DB_USER || "postgres",
+    password: process.env.DB_PASS || "postgres",
+    database: process.env.DB_NAME || "gestor_de_tareas_test",
+    host: process.env.DB_HOST || "127.0.0.1",
+    port: process.env.DB_PORT || 5433,
+    dialect: 'postgres',
+  },
+  production: {
+    username: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT || 5432,
+    dialect: 'postgres',
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false 
+      }
+    }
+  }
+};
